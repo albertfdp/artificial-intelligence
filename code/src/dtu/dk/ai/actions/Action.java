cd34384cf6ef6356541491715ad592b32dcc2d03
Cosmin Stefan <cosminstefanxp@gmail.com>
2013-04-16 14:12:21 +0200

    Eliminated command. Created interface for Actions

--- code/src/dtu/dk/ai/actions/Action.java
+++ code/src/dtu/dk/ai/actions/Action.java
@@ -7,45 +7,16 @@
  */
 package dtu.dk.ai.actions;
 
-import dk.dtu.ai.blueducks.map.Direction;
-
 /**
  * The Class Action.
  */
-public class Action {
-	
-	/**
-	 * Move agent in a direction
-	 * 
-	 * @param dir the direction
-	 * @return 
-	 */
-	public void move(Direction dir){
-		//TODO
-		
-	}
-	
-	/**
-	 * Push a box 
-	 * 
-	 * @param dir_agent direction of the agent
-	 * @param dir_box direction of the box
-	 * @return 
-	 */
-	public void push(Direction dir_agent, Direction dir_box){
-		//TODO
-	}
-	
-	/**
-	 * Pull a box 
-	 * 
-	 * @param dir_agent direction of the agent
-	 * @param current_dir_box the current direction of the box to be pulled 
-	 * @return 
-	 */
-	public void pull(Direction dir_agent, Direction curr_dir_box){
-		//TODO
-	}
+public abstract class Action {
 
+	/**
+	 * Gets the string representing the command to be sent to the server.
+	 * 
+	 * @return the string
+	 */
+	public abstract String toCommandString();
 
 }
