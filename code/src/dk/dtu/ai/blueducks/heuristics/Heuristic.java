5216c70541a086afe63b57cf8cf15c69a4213ce2
rafaelav <rafaela.voiculescu@gmail.com>
2013-04-16 16:52:45 +0200

    Implemented A*. Need to still implement the path return and how the path is kept & updated

--- code/src/dk/dtu/ai/blueducks/heuristics/Heuristic.java
+++ code/src/dk/dtu/ai/blueducks/heuristics/Heuristic.java
@@ -1,6 +1,6 @@
 package dk.dtu.ai.blueducks.heuristics;
 
-public interface Heuristic<StateType, GoalType> {
+public abstract interface Heuristic<StateType, GoalType> {
 
 	public double getHeuristicValue(StateType state, GoalType goal);
 }
