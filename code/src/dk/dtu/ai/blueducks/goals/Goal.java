ed8844366d8d622d13ab2501309feec29ee5febf
Albert Fernández <albertfdp@gmail.com>
2013-04-20 14:38:25 +0200

    Added some text to be able to push. Git is not allowing me to merge and push.

--- code/src/dk/dtu/ai/blueducks/goals/Goal.java
+++ code/src/dk/dtu/ai/blueducks/goals/Goal.java
@@ -1,3 +1,10 @@
+/*
+ * Artificial Intelligence and Multi-Agent Systems
+ * Denmarks Tehnical University
+ * 
+ * Blue Ducks
+ * Spring 2013
+ */
 package dk.dtu.ai.blueducks.goals;
 
 import dk.dtu.ai.blueducks.Agent;
@@ -6,6 +13,14 @@ import dk.dtu.ai.blueducks.map.Cell;
 
 public abstract class Goal {
 		
+	/**
+	 * Gets the action.
+	 *
+	 * @param currentCell the current cell
+	 * @param nextCell the next cell
+	 * @param agent the agent
+	 * @return the action
+	 */
 	public abstract Action getAction (Cell currentCell, Cell nextCell, Agent agent);
 
 }
