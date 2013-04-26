6336fc02489539361aa92baaea33b8909e6e0518
rafaelav <rafaela.voiculescu@gmail.com>
2013-04-26 17:57:31 +0200

    Solved issues with generics

---
+++ code/src/dk/dtu/ai/blueducks/heuristics/MoveBoxHeuristic.java
@@ -0,0 +1,16 @@
+package dk.dtu.ai.blueducks.heuristics;
+
+import dk.dtu.ai.blueducks.goals.MoveBoxGoal;
+import dk.dtu.ai.blueducks.map.State;
+
+public class MoveBoxHeuristic implements Heuristic<State, MoveBoxGoal> {
+
+	@Override
+	public float getHeuristicValue(State state, MoveBoxGoal goal) {
+		float distance = Math.abs(goal.getTo().x - state.getCellForBox(goal.getWhat()).x)
+				+ Math.abs(goal.getTo().y - state.getCellForBox(goal.getWhat()).y);
+
+		return distance;
+	}
+
+}
