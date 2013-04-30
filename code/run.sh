deb7cf48772238ac75b8d1540af5582a3735e71d
Cosmin Stefan <cosminstefanxp@gmail.com>
2013-04-30 15:01:29 +0200

    New run.sh

--- code/run.sh
+++ code/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+LEVEL="levels/FOSAsimple1.lvl"
 if [ $# == '1' ]
 then
     if [[ "$1" = "debug" ]]
     then
-        java -jar server.jar -l levels/FOMAsimple1.lvl -c 'java -classpath bin -Xdebug -Xrunjdwp:transport=dt_socket,server=n,address=8000 dk.dtu.ai.blueducks.BlueDucksClient -Djava.util.logging.SimpleFormatter.format="%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n" '
+        java -jar server.jar -l $LEVEL -c 'java -classpath bin -Xdebug -Xrunjdwp:transport=dt_socket,server=n,address=8000 dk.dtu.ai.blueducks.BlueDucksClient -Djava.util.logging.SimpleFormatter.format="%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n" '
     else
-    	java -jar server.jar -l levels/FOMAsimple1.lvl -c 'java -classpath bin dk.dtu.ai.blueducks.BlueDucksClient -Djava.util.logging.SimpleFormatter.format="%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n" '
+    	java -jar server.jar -l $LEVEL -c 'java -classpath bin dk.dtu.ai.blueducks.BlueDucksClient -Djava.util.logging.SimpleFormatter.format="%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n" '
     fi
 else
-	java -jar server.jar -l levels/FOMAsimple1.lvl -c 'java -classpath bin dk.dtu.ai.blueducks.BlueDucksClient -Djava.util.logging.SimpleFormatter.format="%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n" '
+	java -jar server.jar -l $LEVEL -c 'java -classpath bin dk.dtu.ai.blueducks.BlueDucksClient -Djava.util.logging.SimpleFormatter.format="%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n" '
 fi
