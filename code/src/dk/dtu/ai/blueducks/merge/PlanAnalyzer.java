4c8a01b00e83de6f927359f9b44d650b7a4d2cce
rafaelav <rafaela.voiculescu@gmail.com>
2013-05-26 15:03:07 +0200

    Created PlanAnalyzer where we have the method to find all possible conflicting groups of agents

---
+++ code/src/dk/dtu/ai/blueducks/merge/PlanAnalyzer.java
@@ -0,0 +1,86 @@
+package dk.dtu.ai.blueducks.merge;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+public class PlanAnalyzer {
+
+	public static <T> int getIntersection(Set<T> set1, Set<T> set2) {
+		boolean set1IsLarger = set1.size() > set2.size();
+		Set<T> cloneSet = new HashSet<T>(set1IsLarger ? set2 : set1);
+		cloneSet.retainAll(set1IsLarger ? set1 : set2);
+		return cloneSet.size();
+	}
+
+	public List<CommonResources> findPossibleConflictingAgents(PlanAffectedResources[] affectedResources) {
+		List<CommonResources> commonResources = new LinkedList<CommonResources>();
+
+		// for each agent we test to see if he has something in common with other CommonResources
+		// from other agents
+		for (short agent = 0; agent < affectedResources.length; agent++) {
+
+			// making a CommonResouces object for each agent at the beginning
+			CommonResources agentCommonResources = new CommonResources(agent, affectedResources[agent]);
+
+			Iterator<CommonResources> it = commonResources.iterator();
+
+			// we iterate through all the CommonResources
+			while (it.hasNext()) {
+				// extract the current CommonResources
+				CommonResources c = it.next();
+
+				// figuring out if the resources of the agent conflict with resources from other
+				// groups or agents
+				if (agentCommonResources.intersectWith(c)) {
+					// if yes- we merge them
+					agentCommonResources.merge(c);
+					// and remove the previous ones cause now we have them in the new agent's group
+					it.remove();
+				}
+			}
+
+			// adding to the list of CommonResources the newly created CommonResources which is made
+			// out of all the groups which conflict with the current agent
+			commonResources.add(agentCommonResources);
+		}
+		return commonResources;
+	}
+
+	public class CommonResources {
+		public Set<Short> agents;
+		public PlanAffectedResources resources;
+
+		public CommonResources(short a, PlanAffectedResources res) {
+			this.agents = new HashSet<Short>();
+			this.agents.add(a);
+
+			this.resources = res;
+		}
+
+		public void merge(CommonResources other) {
+			agents.addAll(other.agents);
+			resources.affectedCells.addAll(other.resources.affectedCells);
+			resources.affectedBoxes.addAll(other.resources.affectedBoxes);
+		}
+
+		/**
+		 * @param other - CommonResources
+		 * @return true if the 2 have Cells or Boxes in common in the resources field and false
+		 *         otherwise
+		 */
+		public boolean intersectWith(CommonResources other) {
+			// if there are common cells it will return true
+			if (getIntersection(this.resources.affectedCells, other.resources.affectedCells) != 0)
+				return true;
+			// if there are common boxes it will return true
+			if (getIntersection(this.resources.affectedBoxes, other.resources.affectedBoxes) != 0)
+				return true;
+
+			// there are no common resources so it returns false
+			return false;
+		}
+	}
+}
