34c5de965e8f0e51b863e32b4cc34645d8f97c85
Ruxandra Nistor <roxanne0389@gmail.com>
2013-05-31 00:50:26 +0200

    Added hash for agent, box, cell

--- code/src/dk/dtu/ai/blueducks/Agent.java
+++ code/src/dk/dtu/ai/blueducks/Agent.java
@@ -334,4 +334,33 @@ public class Agent {
 		MotherOdin.getInstance().appendConflictPlan(this, completePlan);
 		this.forbidenCell = null;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Agent other = (Agent) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
 }
