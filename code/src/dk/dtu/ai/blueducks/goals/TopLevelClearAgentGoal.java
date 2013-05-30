4d62132db61df6aa40faba7471a333b88c480c84
Ruxandra Nistor <roxanne0389@gmail.com>
2013-05-30 15:56:50 +0200

    Added equals and hash for TopLevelCells

--- code/src/dk/dtu/ai/blueducks/goals/TopLevelClearAgentGoal.java
+++ code/src/dk/dtu/ai/blueducks/goals/TopLevelClearAgentGoal.java
@@ -31,5 +31,36 @@ public class TopLevelClearAgentGoal extends Goal{
 		return true;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((agentToBeCleared == null) ? 0 : agentToBeCleared.hashCode());
+		result = prime * result + ((cells == null) ? 0 : cells.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TopLevelClearAgentGoal other = (TopLevelClearAgentGoal) obj;
+		if (agentToBeCleared == null) {
+			if (other.agentToBeCleared != null)
+				return false;
+		} else if (!agentToBeCleared.equals(other.agentToBeCleared))
+			return false;
+		if (cells == null) {
+			if (other.cells != null)
+				return false;
+		} else if (!cells.equals(other.cells))
+			return false;
+		return true;
+	}
+
 
 }
